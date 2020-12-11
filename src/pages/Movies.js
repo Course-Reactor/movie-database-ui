6bbbae81b4edcfd23ac9cd498545ff3372793e47
Ryan Coleman <me@ryuhhnn.com>
2020-12-10 18:59:17 -0600

    shhhhh

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,29 +1,7 @@
-import { Row, Col, Card } from "antd";
-import { useQuery } from "react-query";
-import constants from "../constants";
-
-const { Meta } = Card;
+import { Row } from "antd";
 
 const Movies = () => {
-  const { isLoading, isError, data, error } = useQuery("movies", () =>
-    fetch(constants.api.movies.list).then((res) => res.json())
-  );
-
-  if (isLoading) return <span>Loading...</span>;
-  if (isError) return <span>Error: {error.message}</span>;
-
-  return (
-    <Row>
-      {data &&
-        data.map((movie) => (
-          <Col span={12} key={movie.id}>
-            <Card hoverable style={{ margin: 15 }}>
-              <Meta title={movie.title} description={movie.genre} />
-            </Card>
-          </Col>
-        ))}
-    </Row>
-  );
+  return <Row></Row>;
 };
 
 export default Movies;
